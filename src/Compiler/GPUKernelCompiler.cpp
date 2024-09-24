f54d4aa372ca21962b63b37e53939f7e3eb2c6be
Tom Clabault <email>
2024-09-24 19:29:34 +0200

    More compiled kernel infos

--- src/Compiler/GPUKernelCompiler.cpp
+++ src/Compiler/GPUKernelCompiler.cpp
@@ -39,11 +39,16 @@ oroFunction_t GPUKernelCompiler::compile_kernel(GPUKernel& kernel, const GPUKern
 		std::lock_guard<std::mutex> lock(m_mutex);
 		std::cout << "Kernel \"" << kernel_function_name << "\" compiled in " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << "ms. ";
 
-		if (hiprt_orochi_ctx->device_properties.major >= 7 && hiprt_orochi_ctx->device_properties.minor >= 5)
+		if ((hiprt_orochi_ctx->device_properties.major >= 7 && hiprt_orochi_ctx->device_properties.minor >= 5) || std::string(hiprt_orochi_ctx->device_properties.name).find("AMD") != std::string::npos)
+		{
 			// Getting the registers of a kernel only seems to be available on 7.5 and above 
 			// (works on a 2060S but doesn't on a GTX 970 or GTX 1060, couldn't try more hardware 
 			// so maybe 7.5 is too conservative)
-			std::cout << GPUKernel::get_kernel_attribute(kernel_function, ORO_FUNC_ATTRIBUTE_NUM_REGS) << " registers." << std::endl;
+			std::cout << std::endl;
+			std::cout << "\t" << GPUKernel::get_kernel_attribute(kernel_function, ORO_FUNC_ATTRIBUTE_NUM_REGS) << " registers." << std::endl;
+			std::cout << "\t" << GPUKernel::get_kernel_attribute(kernel_function, ORO_FUNC_ATTRIBUTE_SHARED_SIZE_BYTES) << " shared memory." << std::endl;
+			std::cout << "\t" << GPUKernel::get_kernel_attribute(kernel_function, ORO_FUNC_ATTRIBUTE_LOCAL_SIZE_BYTES) << " local memory." << std::endl;
+		}
 		else
 			std::cout << std::endl;
 	}
