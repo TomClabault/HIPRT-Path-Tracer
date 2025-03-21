e6aa49c7b7d9061416f613fdab66df348db1356f
Tom Clabault <email>
2025-03-21 08:50:40 +0100

    Fix specular + diffuse transmission energy conservation

--- src/Scene/SceneParser.cpp
+++ src/Scene/SceneParser.cpp
@@ -436,7 +436,9 @@ void SceneParser::read_material_properties(aiMaterial* mesh_material, CPUMateria
     mesh_material->Get(AI_MATKEY_VOLUME_ATTENUATION_DISTANCE, renderer_material.absorption_at_distance);
     mesh_material->Get(AI_MATKEY_OPACITY, renderer_material.alpha_opacity);
 
-    renderer_material.specular_transmission = 1.0f;
+    /*renderer_material.specular_transmission = 1.0f;
+    renderer_material.roughness = 0.0f;
+    renderer_material.ior = 1.4f;*/
 
     /*renderer_material.specular = 1.0f;
     renderer_material.roughness = 0.0f;
