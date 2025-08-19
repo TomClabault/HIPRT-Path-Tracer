cafaa4062cacc1bf40680eead9d6ee72c4a778b0
Tom Clabault <email>
2025-08-19 19:44:28 +0200

    DRAFT - Gotta fix MIS weights when the per cell distribution isn't able to sample all lights

--- src/Renderer/CPURenderer.cpp
+++ src/Renderer/CPURenderer.cpp
@@ -58,8 +58,8 @@
 // where pixels are not completely independent from each other such as ReSTIR Spatial Reuse).
 // 
 // The neighborhood around pixel will be rendered if DEBUG_RENDER_NEIGHBORHOOD is 1.
-#define DEBUG_PIXEL_X 866
-#define DEBUG_PIXEL_Y 555
+#define DEBUG_PIXEL_X 861
+#define DEBUG_PIXEL_Y 545
 
 // Same as DEBUG_FLIP_Y but for the "other debug pixel"
 #define DEBUG_OTHER_FLIP_Y 0
