4ca078eea438b8e2a82f28901e5a8ce2118b0a1a
Tom Clabault <email>
2025-08-23 17:12:20 +0200

    Fix NEE++ not pre-populating correctly

--- src/Device/kernels/NEE++/GridPrepopulate.h
+++ src/Device/kernels/NEE++/GridPrepopulate.h
@@ -65,8 +65,8 @@ GLOBAL_KERNEL_SIGNATURE(void) inline NEEPlusPlus_Grid_Prepopulate(HIPRTRenderDat
 #endif
 {
 #ifdef __KERNELCC__
-    const uint32_t x = (blockIdx.x * blockDim.x + threadIdx.x) * ReGIR_GridPrepopulationResolutionDownscale;
-    const uint32_t y = (blockIdx.y * blockDim.y + threadIdx.y) * ReGIR_GridPrepopulationResolutionDownscale;
+    const uint32_t x = (blockIdx.x * blockDim.x + threadIdx.x) * NEEPlusPlus_GridPrepoluationResolutionDownscale;
+    const uint32_t y = (blockIdx.y * blockDim.y + threadIdx.y) * NEEPlusPlus_GridPrepoluationResolutionDownscale;
 #endif
     if (x >= render_data.render_settings.render_resolution.x || y >= render_data.render_settings.render_resolution.y)
         return;
