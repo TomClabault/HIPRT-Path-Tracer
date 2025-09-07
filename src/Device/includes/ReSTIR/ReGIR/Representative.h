cb81cfeb04fb367af1fe05453c7ecc77457ad93c
Tom Clabault <email>
2025-09-07 11:22:43 +0200

    Removed HIPRT_INLINE annotations + change HIPRT_HOST_DEVICE to HIPRT_DEVICE in ReGIR Representative.h

--- src/Device/includes/ReSTIR/ReGIR/Representative.h
+++ src/Device/includes/ReSTIR/ReGIR/Representative.h
@@ -11,40 +11,40 @@
 
 #include "HostDeviceCommon/RenderData.h"
 
-HIPRT_HOST_DEVICE float3 ReGIR_get_cell_world_normal(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
+HIPRT_DEVICE float3 ReGIR_get_cell_world_normal(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
 {
 	return render_data.render_settings.regir_settings.get_hash_cell_data_soa(primary_hit).world_normals[hash_grid_cell_index].unpack();
 }
 
-HIPRT_HOST_DEVICE float3 ReGIR_get_cell_world_point(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
+HIPRT_DEVICE float3 ReGIR_get_cell_world_point(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
 {
 	return render_data.render_settings.regir_settings.get_hash_cell_data_soa(primary_hit).world_points[hash_grid_cell_index];
 }
 
-HIPRT_HOST_DEVICE int ReGIR_get_cell_primitive_index(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
+HIPRT_DEVICE int ReGIR_get_cell_primitive_index(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
 {
 	return render_data.render_settings.regir_settings.get_hash_cell_data_soa(primary_hit).hit_primitive[hash_grid_cell_index];
 }
 
-HIPRT_HOST_DEVICE float ReGIR_get_cell_roughness(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
+HIPRT_DEVICE float ReGIR_get_cell_roughness(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
 {
 	// / 255.0f to convert from uchar [0, 255] to float [0, 1]
 	return render_data.render_settings.regir_settings.get_hash_cell_data_soa(primary_hit).roughness[hash_grid_cell_index] / 255.0f;
 }
 
-HIPRT_HOST_DEVICE float ReGIR_get_cell_metallic(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
+HIPRT_DEVICE float ReGIR_get_cell_metallic(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
 {
 	// / 255.0f to convert from uchar [0, 255] to float [0, 1]
 	return render_data.render_settings.regir_settings.get_hash_cell_data_soa(primary_hit).metallic[hash_grid_cell_index] / 255.0f;
 }
 
-HIPRT_HOST_DEVICE float ReGIR_get_cell_specular(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
+HIPRT_DEVICE float ReGIR_get_cell_specular(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
 {
 	// / 255.0f to convert from uchar [0, 255] to float [0, 1]
 	return render_data.render_settings.regir_settings.get_hash_cell_data_soa(primary_hit).specular[hash_grid_cell_index] / 255.0f;
 }
 
-HIPRT_HOST_DEVICE ReGIRGridFillSurface ReGIR_get_cell_surface(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
+HIPRT_DEVICE ReGIRGridFillSurface ReGIR_get_cell_surface(const HIPRTRenderData& render_data, int hash_grid_cell_index, bool primary_hit)
 {
 	int cell_primitive_index = ReGIR_get_cell_primitive_index(render_data, hash_grid_cell_index, primary_hit);
 	float3 cell_point = ReGIR_get_cell_world_point(render_data, hash_grid_cell_index, primary_hit);
@@ -67,7 +67,7 @@ HIPRT_HOST_DEVICE ReGIRGridFillSurface ReGIR_get_cell_surface(const HIPRTRenderD
 /**
  *	Updates the representative point and normal (and other data) of the cell at the given shading point
  */
-HIPRT_HOST_DEVICE void ReGIR_update_representative_data(HIPRTRenderData& render_data, float3 shading_point, float3 surface_normal, const HIPRTCamera& current_camera, int primitive_index, bool primary_hit, const DeviceUnpackedEffectiveMaterial& material)
+HIPRT_DEVICE void ReGIR_update_representative_data(HIPRTRenderData& render_data, float3 shading_point, float3 surface_normal, const HIPRTCamera& current_camera, int primitive_index, bool primary_hit, const DeviceUnpackedEffectiveMaterial& material)
 {
 	if (DirectLightSamplingBaseStrategy != LSS_BASE_REGIR)
 		return;
