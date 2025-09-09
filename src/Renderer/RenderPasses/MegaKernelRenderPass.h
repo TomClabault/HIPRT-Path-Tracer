f25cee9ba01b3f97352e4e150da27dc1dad75abf
Tom Clabault (Linux) <clabaultt@gmail.com>
2025-09-09 18:32:23 +0200

    Fix missing override specifiers

--- src/Renderer/RenderPasses/MegaKernelRenderPass.h
+++ src/Renderer/RenderPasses/MegaKernelRenderPass.h
@@ -18,14 +18,14 @@ public:
 	MegaKernelRenderPass(GPURenderer* renderer);
 	MegaKernelRenderPass(GPURenderer* renderer, const std::string& name);
 
-	virtual void resize(unsigned int new_width, unsigned int new_height);
+	virtual void resize(unsigned int new_width, unsigned int new_height) override;
 	
 	virtual bool pre_render_update(float delta_time) override;
 	virtual bool launch_async(HIPRTRenderData& render_data, GPUKernelCompilerOptions& compiler_options) override;
 	virtual void post_sample_update_async(HIPRTRenderData& render_data, GPUKernelCompilerOptions& compiler_options) override {};
 
-	virtual void update_render_data() {};
-	virtual void reset(bool reset_by_camera_movement);
+	virtual void update_render_data()  override {};
+	virtual void reset(bool reset_by_camera_movement) override;
 
 	virtual bool is_render_pass_used() const override;
 
