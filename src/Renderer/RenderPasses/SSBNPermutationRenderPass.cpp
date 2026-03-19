abb3ec1a05604a7b0654985ddcef7f97f00d2c57
Tom Clabault <email>
2026-03-19 18:07:37 +0100

    Fix broken retargeting pass input buffer pointer

--- src/Renderer/RenderPasses/SSBNPermutationRenderPass.cpp
+++ src/Renderer/RenderPasses/SSBNPermutationRenderPass.cpp
@@ -210,13 +210,14 @@ void SSBNPermutationRenderPass::post_sample_update_async(HIPRTRenderData& render
 												  &m_blue_noise_texture_width,
 												  &m_blue_noise_texture_height,
 												  &render_data.buffers.get_input_random_seeds_pointer(),
-												  &render_data.buffers.get_input_random_seeds_pointer(),
+												  &sorted_seeds_buffer_pointer,
 												  &hash_grid_offsets_buffer_pointer };
 
 		m_kernels[SSBNPermutationRenderPass::SSBN_PERMUTATION_SORTING_PASS]->launch_asynchronous(block_size * block_size, 1,
 																								 block_size * block_size * m_different_hash_count, 1,
 																								 launch_args_sorting, m_renderer->get_main_stream());
 
+		std::vector<int> retargeting_data				   = m_blue_noise_retargeting_texture_buffer.download_data();
 		int* blue_noise_retargeting_texture_buffer_pointer = m_blue_noise_retargeting_texture_buffer.get_device_pointer();
 		void* launch_args_retargeting[]					   = { &render_data,
 															   &blue_noise_retargeting_texture_buffer_pointer,
