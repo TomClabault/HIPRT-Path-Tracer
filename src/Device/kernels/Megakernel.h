a5af4632bb20f59b6b1d7e5c3f1bfbe5795b2449
Tom Clabault <email>
2026-03-21 17:05:01 +0100

    Fix accumulation SSBN permutation pass

--- src/Device/kernels/Megakernel.h
+++ src/Device/kernels/Megakernel.h
@@ -136,14 +136,14 @@ GLOBAL_KERNEL_SIGNATURE(void) inline MegaKernel(HIPRTRenderData render_data, int
 	render_data.aux_buffers.still_one_ray_active[0] = 1;
 
 	path_tracing_accumulate_debug_view_color(render_data, ray_payload, pixel_index, random_number_generator);
-	{
+	/*{
 		ColorRGB32F debug_colors[6] = { ColorRGB32F(1.0e30f, 0.0f, 0.0f),	 ColorRGB32F(0.0f, 1.0e30f, 0.0f),	  ColorRGB32F(0.0f, 0.0f, 1.0e30f),
 										ColorRGB32F(1.0e30f, 1.0e30f, 0.0f), ColorRGB32F(1.0e30f, 0.0f, 1.0e30f), ColorRGB32F(0.0f, 1.0e30f, 1.0e30f) };
 		if (render_data.get_input_random_seed(pixel_index) <= 5)
 			path_tracing_accumulate_color(render_data, debug_colors[render_data.get_input_random_seed(pixel_index)], pixel_index);
 		else
 			path_tracing_accumulate_color(render_data, ColorRGB32F(render_data.get_input_random_seed(pixel_index) / (float)((unsigned int)(-1))), pixel_index);
-	}
+	}*/
 	// path_tracing_accumulate_color(render_data, ColorRGB32F(render_data.get_input_random_seed(pixel_index) / (float)((unsigned int)(-1))), pixel_index);
 
 	path_tracing_accumulate_color(render_data, ray_payload.ray_color, pixel_index);
