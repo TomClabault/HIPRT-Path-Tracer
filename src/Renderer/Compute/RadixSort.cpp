cf3e810febfc9b57ee5c683d328dae76963f3127
Tom Clabault <email>
2026-03-29 17:48:29 +0200

    Fix unsafe last resize element count comparison typo in RadixSort.cpp

--- src/Renderer/Compute/RadixSort.cpp
+++ src/Renderer/Compute/RadixSort.cpp
@@ -98,7 +98,7 @@ void RadixSort::upload_input_data(const std::vector<unsigned int>& keys, const s
 
 void RadixSort::set_data_pointers(unsigned int* keys_device_pointer, unsigned int* values_device_pointer, size_t element_count)
 {
-	if (!m_last_resize_element_count < element_count)
+	if (m_last_resize_element_count < element_count)
 	{
 		g_imgui_logger.add_line(ImGuiLoggerSeverity::IMGUI_LOGGER_ERROR,
 								"RadixSort::set_data_pointers() called with an element_count (%u) that is different from the last one used in resize() (%u). "
@@ -225,7 +225,7 @@ void RadixSort::unit_test(std::shared_ptr<HIPRTOrochiCtx> hiprt_ctx, oroStream_t
 	{
 		rng.seed(i);
 
-		unsigned int test_size = rng() % 10000000;
+		unsigned int test_size = 1280 * 720;
 
 		std::vector<unsigned int> input_keys(test_size);
 		std::vector<unsigned int> input_values(test_size);
