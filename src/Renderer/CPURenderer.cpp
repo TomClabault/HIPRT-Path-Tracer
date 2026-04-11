dfb4dbde9ef676d1a145e296a035034b4b8dfc46
Tom Clabault <email>
2026-04-11 04:13:48 +0200

    Fix #elif condition for CPURenderer light tree builds

--- src/Renderer/CPURenderer.cpp
+++ src/Renderer/CPURenderer.cpp
@@ -344,7 +344,8 @@ void CPURenderer::set_scene(Scene& parsed_scene)
 	m_light_tree_builder_ats.to_device(m_render_data, parsed_scene.emissive_triangles_primitive_indices, parsed_scene.triangles_vertex_indices.size() / 3,
 									   m_light_tree_ats_device_data);
 	m_light_tree_builder_ats.cleanup();
-#elif DirectLightSamplingStrategy == LSS_BASE_LIGHT_TREE_SG ||                                                                                                 \
+#endif
+#if DirectLightSamplingStrategy == LSS_BASE_LIGHT_TREE_SG ||                                                                                                   \
 						(DirectLightUseNEEPlusPlus == KERNEL_OPTION_TRUE && NEEPlusPlusGridPrepopulateLightSamplingStrategy == LSS_BASE_LIGHT_TREE_SG)
 	m_light_tree_builder_sg.build_light_tree(parsed_scene.emissive_triangles_primitive_indices, parsed_scene.triangles_vertex_indices,
 											 parsed_scene.vertices_positions, parsed_scene.material_indices, parsed_scene.materials);
