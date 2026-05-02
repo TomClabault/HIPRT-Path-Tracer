ec128f1b784802da952de236a1ac0b51fdd0f968
Tom Clabault <email>
2026-05-02 09:38:18 +0200

    Fix ReSTIR PT temporal reuse crash

--- src/HostDeviceCommon/RenderSettings.cpp
+++ src/HostDeviceCommon/RenderSettings.cpp
@@ -18,7 +18,7 @@ HIPRT_HOST bool HIPRTRenderSettings::use_prev_frame_g_buffer(GPURenderer* render
 
 	if (renderer->get_ReSTIR_GI_render_pass())
 		need_g_buffer |= renderer->get_ReSTIR_GI_render_pass()->is_render_pass_used() && restir_gi_settings.common_temporal_pass.do_temporal_reuse_pass;
-	else if (renderer->get_ReSTIR_PT_render_pass())
+	if (renderer->get_ReSTIR_PT_render_pass())
 		need_g_buffer |= renderer->get_ReSTIR_PT_render_pass()->is_render_pass_used() && restir_pt_settings.common_temporal_pass.do_temporal_reuse_pass;
 
 	return need_g_buffer;
