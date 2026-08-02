7b7e14e2cbc949a474fc2cfb6fd8a7a71afe79bf
Tom Clabault <email>
2026-08-02 11:10:26 +0200

    Update default mean_radiance_threshold

--- src/Device/includes/IlluminationAwareKDTree/IlluminationAwareKDTreeUserSettings.h
+++ src/Device/includes/IlluminationAwareKDTree/IlluminationAwareKDTreeUserSettings.h
@@ -12,7 +12,7 @@ struct IlluminationAwareKDTreeUserSettings
 {
 	int minimum_sample_count_for_lookahead_creation = 1000;
 	int minimum_sample_count_for_splitting			= 1000;
-	float mean_radiance_split_threshold				= 0.2f;
+	float mean_radiance_split_threshold				= 0.15f;
 
 	IlluminationAwareKDTreeSubdivisionMode subdivision_mode = IlluminationAwareKDTreeSubdivisionMode::MEAN_RADIANCE_ONLY;
 };
