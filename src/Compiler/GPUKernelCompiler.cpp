95aec8852f1410c6aba83ee3b942e4d904326e81
Tom Clabault <email>
2026-08-11 16:42:51 +0200

    Better wording for kernel statistics [Registers, Shared memory, Local memory]

--- src/Compiler/GPUKernelCompiler.cpp
+++ src/Compiler/GPUKernelCompiler.cpp
@@ -129,9 +129,9 @@ oroFunction_t GPUKernelCompiler::compile_kernel(GPUKernel& kernel,
 		int nb_shared = GPUKernel::get_kernel_attribute(kernel_function, ORO_FUNC_ATTRIBUTE_SHARED_SIZE_BYTES);
 		int nb_local  = GPUKernel::get_kernel_attribute(kernel_function, ORO_FUNC_ATTRIBUTE_LOCAL_SIZE_BYTES);
 
-		g_imgui_logger.add_line(ImGuiLoggerSeverity::IMGUI_LOGGER_INFO, "Kernel \"%s\" compiled in %ldms.\n\t[Reg, Shared, Local] = [%d, %d, %d]\n",
-								kernel.get_kernel_name().c_str(), std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count(), nb_reg,
-								nb_shared, nb_local);
+		g_imgui_logger.add_line(
+			ImGuiLoggerSeverity::IMGUI_LOGGER_INFO, "Kernel \"%s\" compiled in %ldms.\n\t[Registers, Shared memory, Local memory] = [%d, %d, %d]\n",
+			kernel.get_kernel_name().c_str(), std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count(), nb_reg, nb_shared, nb_local);
 	}
 
 	return kernel_function;
