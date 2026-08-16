db5480de80bb6bd0efb299e39f81d7f3bda21664
Tom Clabault <email>
2026-08-16 16:15:22 +0200

    Remove all maximum diversity heuristics for NISLM SG importance caches representatives selection, just keep the simplest one

--- src/Compiler/GPUKernelCompilerOptions.cpp
+++ src/Compiler/GPUKernelCompilerOptions.cpp
@@ -120,8 +120,6 @@ const std::string GPUKernelCompilerOptions::LIGHT_TREE_SG_USE_TREE_CUT = "LightT
 const std::string GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_MAXIMUM_LOOKAHEAD_LEVEL_COUNT = "IlluminationAwareKDTreeMaximumLookaheadLevelCount";
 const std::string GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_DEBUG_MODE = "IlluminationAwareKDTreeDebugMode";
 const std::string GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_DEBUG_REPRESENTATIVE_POINTS = "IlluminationAwareKDTreeDebugRepresentativePoints";
-const std::string GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC =
-	"IlluminationAwareKDTreeNISMLNormalDiversityHeuristic";
 
 const std::string GPUKernelCompilerOptions::NISML_USE_SG_IMPORTANCES_KD_TREE_CACHES = "NISMLUseSGImportancesKDTreeCaches";
 const std::string GPUKernelCompilerOptions::NISML_DEBUG_MODE = "NISMLDebugMode";
@@ -263,7 +261,6 @@ const std::unordered_set<std::string> GPUKernelCompilerOptions::ALL_MACROS_NAMES
 	GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_MAXIMUM_LOOKAHEAD_LEVEL_COUNT,
 	GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_DEBUG_MODE,
 	GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_DEBUG_REPRESENTATIVE_POINTS,
-	GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC,
 
 	GPUKernelCompilerOptions::NISML_USE_SG_IMPORTANCES_KD_TREE_CACHES,
 	GPUKernelCompilerOptions::NISML_DEBUG_MODE,
@@ -411,8 +408,6 @@ GPUKernelCompilerOptions::GPUKernelCompilerOptions()
 	m_options_macro_map[GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_DEBUG_MODE] = std::make_shared<int>(IlluminationAwareKDTreeDebugMode);
 	m_options_macro_map[GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_DEBUG_REPRESENTATIVE_POINTS] =
 		std::make_shared<int>(IlluminationAwareKDTreeDebugRepresentativePoints);
-	m_options_macro_map[GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC] =
-		std::make_shared<int>(IlluminationAwareKDTreeNISMLNormalDiversityHeuristic);
 
 	m_options_macro_map[GPUKernelCompilerOptions::NISML_USE_SG_IMPORTANCES_KD_TREE_CACHES] = std::make_shared<int>(NISMLUseSGImportancesKDTreeCaches);
 	m_options_macro_map[GPUKernelCompilerOptions::NISML_DEBUG_MODE] = std::make_shared<int>(NISMLDebugMode);
--- src/UI/ImGui/ImGuiSettingsWindow.cpp
+++ src/UI/ImGui/ImGuiSettingsWindow.cpp
@@ -2478,7 +2478,7 @@ void ImGuiSettingsWindow::draw_ReSTIR_PG_settings_panel()
 	ReSTIRPGSettings& restir_pg_settings							= render_settings.restir_pg_settings;
 	std::shared_ptr<GPUKernelCompilerOptions> global_kernel_options = m_renderer->get_global_compiler_options();
 	std::shared_ptr<ReSTIRPGRenderPass> restir_pg_render_pass		= std::dynamic_pointer_cast<ReSTIRPGRenderPass>(
-		m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(ReSTIRPGRenderPass::RESTIR_PG_RENDER_PASS_NAME));
+		  m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(ReSTIRPGRenderPass::RESTIR_PG_RENDER_PASS_NAME));
 
 	if (ImGui::CollapsingHeader("ReSTIR PG"))
 	{
@@ -2659,7 +2659,7 @@ void ImGuiSettingsWindow::draw_ReGIR_settings_panel()
 	HIPRTRenderData& render_data									= m_renderer->get_render_data();
 	std::shared_ptr<GPUKernelCompilerOptions> global_kernel_options = m_renderer->get_global_compiler_options();
 	std::shared_ptr<ReGIRRenderPass> regir_render_pass				= std::dynamic_pointer_cast<ReGIRRenderPass>(
-		m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(ReGIRRenderPass::REGIR_RENDER_PASS_NAME));
+		 m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(ReGIRRenderPass::REGIR_RENDER_PASS_NAME));
 
 	ImGui::BeginDisabled(!regir_render_pass);
 	if (ImGui::CollapsingHeader("ReGIR Settings") && regir_render_pass)
@@ -3740,7 +3740,7 @@ void ImGuiSettingsWindow::draw_light_tree_SG_settings_panel()
 	std::shared_ptr<GPUKernelCompilerOptions> global_kernel_options							  = m_renderer->get_global_compiler_options();
 	std::shared_ptr<IlluminationAwareKDTreeRenderPass> illumination_aware_kd_tree_render_pass = m_renderer->get_illumination_aware_kd_tree_render_pass();
 	std::shared_ptr<NISMLRenderPass> nisml_render_pass										  = std::dynamic_pointer_cast<NISMLRenderPass>(
-		m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(NISMLRenderPass::NISML_RENDER_PASS_NAME));
+		   m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(NISMLRenderPass::NISML_RENDER_PASS_NAME));
 
 	int direct_light_nee_estimator	   = global_kernel_options->get_macro_value(GPUKernelCompilerOptions::DIRECT_LIGHT_NEE_ESTIMATOR);
 	int direct_light_sampling_strategy = global_kernel_options->get_macro_value(GPUKernelCompilerOptions::DIRECT_LIGHT_SAMPLING_STRATEGY);
@@ -4024,7 +4024,7 @@ void ImGuiSettingsWindow::draw_illumination_aware_kd_tree_panel()
 	std::shared_ptr<GPUKernelCompilerOptions> global_kernel_options							  = m_renderer->get_global_compiler_options();
 	std::shared_ptr<IlluminationAwareKDTreeRenderPass> illumination_aware_kd_tree_render_pass = m_renderer->get_illumination_aware_kd_tree_render_pass();
 	std::shared_ptr<NISMLRenderPass> nisml_render_pass										  = std::dynamic_pointer_cast<NISMLRenderPass>(
-		m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(NISMLRenderPass::NISML_RENDER_PASS_NAME));
+		   m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(NISMLRenderPass::NISML_RENDER_PASS_NAME));
 
 	int direct_light_nee_estimator		  = global_kernel_options->get_macro_value(GPUKernelCompilerOptions::DIRECT_LIGHT_NEE_ESTIMATOR);
 	int direct_light_sampling_strategy	  = global_kernel_options->get_macro_value(GPUKernelCompilerOptions::DIRECT_LIGHT_SAMPLING_STRATEGY);
@@ -4097,11 +4097,11 @@ void ImGuiSettingsWindow::draw_illumination_aware_kd_tree_panel()
 				std::size_t nee_training_buffer_bytes = vram_usage.nee_training_records + vram_usage.nee_training_record_count;
 				std::size_t final_distribution_bytes  = vram_usage.tree_cut_sampling_probabilities + vram_usage.tree_cut_sampling_cdfs;
 				std::size_t per_cell_history_bytes	  = vram_usage.history_per_cell_sample_count + vram_usage.history_per_cell_normal_sum_x +
-														vram_usage.history_per_cell_normal_sum_y + vram_usage.history_per_cell_normal_sum_z +
-														vram_usage.history_per_cell_normal_count;
-				std::size_t per_cut_history_bytes	  = vram_usage.history_per_cut_node_estimated_second_moment + vram_usage.history_per_cut_node_sample_count;
-				std::size_t per_cut_batch_bytes		  = vram_usage.batch_per_cut_node_second_moment_sum + vram_usage.batch_per_cut_node_sample_count;
-				std::size_t prior_distribution_bytes  = vram_usage.tree_cut_sampling_prior_pdfs + vram_usage.tree_cut_sampling_prior_cdfs;
+													 vram_usage.history_per_cell_normal_sum_y + vram_usage.history_per_cell_normal_sum_z +
+													 vram_usage.history_per_cell_normal_count;
+				std::size_t per_cut_history_bytes	 = vram_usage.history_per_cut_node_estimated_second_moment + vram_usage.history_per_cut_node_sample_count;
+				std::size_t per_cut_batch_bytes		 = vram_usage.batch_per_cut_node_second_moment_sum + vram_usage.batch_per_cut_node_sample_count;
+				std::size_t prior_distribution_bytes = vram_usage.tree_cut_sampling_prior_pdfs + vram_usage.tree_cut_sampling_prior_cdfs;
 
 				illumination_aware_vram_tooltip += std::format(
 					"  - NEE distribution training buffers: {:.3f}MB\n"
@@ -4314,7 +4314,7 @@ void ImGuiSettingsWindow::draw_neural_many_lights_panel()
 	std::shared_ptr<GPUKernelCompilerOptions> global_kernel_options							  = m_renderer->get_global_compiler_options();
 	std::shared_ptr<IlluminationAwareKDTreeRenderPass> illumination_aware_kd_tree_render_pass = m_renderer->get_illumination_aware_kd_tree_render_pass();
 	std::shared_ptr<NISMLRenderPass> nisml_render_pass										  = std::dynamic_pointer_cast<NISMLRenderPass>(
-		m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(NISMLRenderPass::NISML_RENDER_PASS_NAME));
+		   m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(NISMLRenderPass::NISML_RENDER_PASS_NAME));
 
 	int direct_light_nee_estimator	   = global_kernel_options->get_macro_value(GPUKernelCompilerOptions::DIRECT_LIGHT_NEE_ESTIMATOR);
 	int direct_light_sampling_strategy = global_kernel_options->get_macro_value(GPUKernelCompilerOptions::DIRECT_LIGHT_SAMPLING_STRATEGY);
@@ -4466,30 +4466,6 @@ void ImGuiSettingsWindow::draw_neural_many_lights_panel()
 							m_render_window->set_render_dirty(true);
 						}
 						ImGuiRenderer::show_help_marker("Normal quantization precision used in NISML SG-importance cache keys.");
-
-						int nisml_normal_diversity_heuristic =
-							global_kernel_options->get_macro_value(GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC);
-						bool nisml_normal_diversity_heuristic_changed = false;
-						nisml_normal_diversity_heuristic_changed |= ImGui::RadioButton("No local heuristic##nisml", &nisml_normal_diversity_heuristic,
-																					   ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC_NONE);
-						nisml_normal_diversity_heuristic_changed |=
-							ImGui::RadioButton("Current local min-max heuristic##nisml", &nisml_normal_diversity_heuristic,
-											   ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC_LOCAL);
-						nisml_normal_diversity_heuristic_changed |=
-							ImGui::RadioButton("Full correct O(N^2) heuristic##nisml", &nisml_normal_diversity_heuristic,
-											   ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC_FULL);
-						if (nisml_normal_diversity_heuristic_changed)
-						{
-							global_kernel_options->set_macro_value(GPUKernelCompilerOptions::ILLUMINATION_AWARE_KD_TREE_NISML_NORMAL_DIVERSITY_HEURISTIC,
-																   nisml_normal_diversity_heuristic);
-
-							m_renderer->recompile_kernels();
-							m_render_window->set_render_dirty(true);
-						}
-						ImGuiRenderer::show_help_marker(
-							"Select how overflow representatives are chosen: retain the original random reservoir algorithm, use the local min-max "
-							"heuristic, or evaluate every possible replacement and minimize the resulting maximum pairwise normal similarity. "
-							"Changing this recompiles the kernels.");
 					}
 				}
 
@@ -5995,7 +5971,7 @@ void ImGuiSettingsWindow::draw_post_process_panel()
 
 	std::shared_ptr<GPUKernelCompilerOptions> global_kernel_options = m_renderer->get_global_compiler_options();
 	std::shared_ptr<GMoNRenderPass> gmon_render_pass				= std::dynamic_pointer_cast<GMoNRenderPass>(
-		m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(GMoNRenderPass::GMON_RENDER_PASS_NAME));
+		   m_renderer->get_render_graphs()[GPURendererThread::RENDER_GRAPH_FULL_NAME].get_render_pass(GMoNRenderPass::GMON_RENDER_PASS_NAME));
 	GMoNGPUData& gmon_data = gmon_render_pass->get_gmon_data();
 
 	if (!render_data.render_settings.accumulate)
