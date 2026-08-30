d7e1a9f07382965332afe8f7767952310f785fbe
Tom Clabault <email>
2026-08-30 14:17:28 +0200

    Fix wrong use of m_renderer->* in IlluminationAwareKDTreeRenderPass::post_sample_update_async()

--- src/Renderer/RenderPasses/IlluminationAwareKDTreeRenderPass.cpp
+++ src/Renderer/RenderPasses/IlluminationAwareKDTreeRenderPass.cpp
@@ -531,7 +531,7 @@ void IlluminationAwareKDTreeRenderPass::post_sample_update_async(HIPRTRenderData
 			256, 1, kd_tree_device.learning_to_cluster.training_sample_capacity, 1, learning_to_cluster_launch_args, m_renderer->get_main_stream());
 
 		unsigned int learning_to_cluster_lightcut_block_size =
-			m_renderer->get_global_compiler_options()->get_macro_value(GPUKernelCompilerOptions::LEARNING_TO_CLUSTER_MAXIMUM_LIGHT_CUT_SIZE);
+			static_cast<unsigned int>(compiler_options.get_macro_value(GPUKernelCompilerOptions::LEARNING_TO_CLUSTER_MAXIMUM_LIGHT_CUT_SIZE));
 		unsigned int maximum_lightcut_work_count = kd_tree_device.core.node_capacity * SurfaceNormalFace_Count * learning_to_cluster_lightcut_block_size;
 		m_kernels[IlluminationAwareKDTreeRenderPass::LEARNING_TO_CLUSTER_ALLOCATE_NORMAL_FACE_LIGHTCUTS_KERNEL_ID]->launch_asynchronous(
 			learning_to_cluster_lightcut_block_size, 1, maximum_lightcut_work_count, 1, learning_to_cluster_launch_args, m_renderer->get_main_stream());
